3d97e8b0e73e79667df8ee4d56baa96d174140c1
LuuckyG <luuk.geelen@hotmail.com>
2020-03-16 17:23:34 +0100

    Separated model building and text generation function from training script

--- model.py
+++ model.py
@@ -1,150 +1,60 @@
-import os
-import time
-import math
-import pickle
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-
-from pathlib import Path
-from datetime import datetime
-from pickle import load, dump
-
-# Neural Net Preprocessing
-from sklearn.externals import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-# Neural Net Layers
 from tensorflow.keras.models import Sequential
+
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Embedding
 
-# Neural Net Training
-from tensorflow.keras.models import load_model
-from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, TensorBoard
-from keras.callbacks import EarlyStopping
-
-# Import the data
-dataset = pd.read_excel("database.xlsx")
-subject = 'AANLEIDING'
-text = dataset[dataset['Zwaartepunt'] == 'Programmatuur'][subject].values
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
-# path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-# text = open(path_to_file, 'rb').read().decode(encoding='cp1252')
-vocab = sorted(set(text))
+def build_model(num_cells, 
+                rnn_layers, 
+                vocab_size, 
+                train_len, 
+                dropout=0.3,
+                optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy']):
 
-# Creating a mapping from unique characters to indices
-char2idx = {u:i for i, u in enumerate(vocab)}
-idx2char = np.array(vocab)
+    """Function to build and return a recurrent LSTM-based model"""
 
-text_as_int = np.array([char2idx[c] for c in text])
+    # Define model
+    model = Sequential()
+    model.add(Embedding(vocab_size + 1, 25, input_length=train_len))
 
-max_words = 50000  # Max size of the dictionary
-tokenizer = Tokenizer(num_words=max_words)
-tokenizer.fit_on_texts(text)
-sequences = tokenizer.texts_to_sequences(dataset[subject].values)
-print(sequences[:5])
+    for n in range(rnn_layers):
+        if n != (rnn_layers - 1):
+            model.add(LSTM(num_cells), return_sequences=True)
+            model.add(Dropout(dropout))
+        else:
+            # Last LSTM layer
+            model.add(LSTM(num_cells))
+    
+    model.add(Dense(num_cells), activation=activation)
+    model.add(Dropout(dropout))
+    model.add(Dense(vocab_size + 1, activation='softmax'))
 
-# Flatten the list of lists resulting from the tokenization. This will reduce the list
-# to one dimension, allowing us to apply the sliding window technique to predict the next word
-text = [item for sublist in sequences for item in sublist]
-vocab_size = len(tokenizer.word_index)
+    # Print summary
+    model.summary()
 
-print('Vocabulary size in this corpus: ', vocab_size)
-
-# Training on 19 words to predict the 20th
-sentence_len = 20
-pred_len = 1
-train_len = sentence_len - pred_len
-seq = []
-
-# Sliding window to generate train data
-for i in range(len(text) - sentence_len):
-    seq.append(text[i:i + sentence_len])
-
-# Reverse dictionary to decode tokenized sequences back to words
-reverse_word_map = dict(map(reversed, tokenizer.word_index.items()))
-
-# Each row in seq is a 20 word long window. We append he first 19 words as the input to predict the 20th word
-X = []
-y = []
-for i in seq:
-    X.append(i[:train_len])
-    y.append(i[-1])
-
-y = to_categorical(y)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
-
-X_val = np.asarray(X_train[-int(len(X_train) * 0.2):])  # Get last 20% of training data for validation
-y_val =  np.asarray(y_train[-int(len(y_train) * 0.2):])
-X_train =  np.asarray(X_train[:-int(len(X_train) * 0.2)])
-y_train =  np.asarray(y_train[:-int(len(y_train) * 0.2)])
-X_test =  np.asarray(X_test)
-y_test =  np.asarray(y_test)
-
-# Hyperparameters
-learning_rate = 0.003
-loss_type = 'categorical_crossentropy'
-metrics = ['accuracy']
-optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-
-# define model
-model = Sequential([
-    Embedding(vocab_size + 1, 25, input_length=train_len),
-    LSTM(256, return_sequences=True),
-    Dropout(0.3),
-    LSTM(256, return_sequences=True),
-    Dropout(0.3),
-    LSTM(256),
-    Dense(256, activation='relu'),
-    Dropout(0.3),
-    Dense(vocab_size + 1, activation='softmax')
-])
-
-model.summary()
-
-model.compile(optimizer=optimizer,
-              loss=loss_type,
-              metrics=metrics)
-
-time_now = datetime.now()
-results_path = Path('./results')
-results_directory = results_directory = results_path / ''.join(optimizer_type + '_' + loss_type + '_' + str(learning_rate) + '_' + time_now.strftime('%H-%M'))
-results_directory.mkdir(exist_ok=True, parents=True)
-
-checkpoint = ModelCheckpoint(results_path / ''.join("model_2_weights.hdf5"), monitor='loss', verbose=1, save_best_only=False, mode='min')
-callbacks_list = [checkpoint]
- 
-# Tensorboard logger.
-tensorboard = TensorBoard(log_dir=results_directory, write_graph=True, write_grads=False, write_images=True)
-
-history = model.fit(X_train,
-                    y_train,
-                    epochs=200,
-                    batch_size=128,
-                    callbacks=callbacks_list,
-                    verbose=1,
-                    validation_data=(X_val, y_val))
-
-# Save tokenizer
-pickle.dump(tokenizer, open('{}/tokenizer.pkl'.format(results_directory), 'wb'))
-joblib.dump(model, '{}/{}_model.pkl'.format(results_directory, subject))
-model.save('{}/model_weights.hdf5'.format(results_directory))
+    # Build model
+    model.compile(optimizer=optimizer,
+                  loss=loss,
+                  metrics=metrics)
+    
+    return model
 
 
-def gen(model, seq, max_len=20):
+def generate_text(model, seq, max_words=50000, max_len=20):
     """ Generates a sequence given a string seq using specified model until the total sequence length
     reaches max_len"""
     # Tokenize the input string
+    tokenizer = Tokenizer(num_words=max_words)
     tokenized_sent = tokenizer.texts_to_sequences([seq])
     max_len = max_len + len(tokenized_sent[0])
+
     # If sentence is not as long as the desired sentence length, we need to 'pad sequence' so that
     # the array input shape is correct going into our LSTM. the `pad_sequences` function adds
     # zeroes to the left side of our sequence until it becomes 19 long, the number of input features.
@@ -154,8 +64,3 @@ def gen(model, seq, max_len=20):
         tokenized_sent[0].append(op.argmax() + 1)
 
     return " ".join(map(lambda x: reverse_word_map[x], tokenized_sent[0]))
-
-
-test_string = 'De aanvrager'
-sequence_length = 15
-gen(model, test_string, sequence_length)
