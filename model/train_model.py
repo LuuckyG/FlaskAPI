c1075d23e5b20067c496f96f38e36c8fbee605a1
LuuckyG <luuk.geelen@hotmail.com>
2020-03-30 17:11:24 +0200

    Small bug fixes in inference and text generation

--- model/train_model.py
+++ model/train_model.py
@@ -116,7 +116,7 @@ def train_model(results_path: Path, path_to_file: str, cfg: dict):
                         batch_size=cfg['batch_size'],
                         callbacks=callbacks_list,
                         verbose=2,
-                        validation_data=(X_val, y_val))    
+                        validation_data=(X_val, y_val))
 
     cfg = save_config(model, tokenizer, cfg, reverse_word_map, results_directory)
     return model, cfg
