2d80295caa7e94045c3287cafe58b7f4b958f672
LuuckyG <luuk.geelen@hotmail.com>
2020-04-09 15:36:57 +0200

    Removed simple model

--- model/train_model.py
+++ model/train_model.py
@@ -29,12 +29,10 @@ from tensorflow.keras.callbacks import LearningRateScheduler, Callback
 
 # Own scripts
 from hparams import HParams
-from .models import build_simple_model, generate_text
 from utils.preprocess import split_data
 from utils.logging import checkpoint_log, save_config
 from .utils import textgenrnn_encode_cat
 from .textgenrnn import textgenrnn
-from .textgenrnn2 import TextGeneration
 
 
 def train_model(results_path: Path, path_to_file: str, cfg: dict):
@@ -54,57 +52,8 @@ def train_model(results_path: Path, path_to_file: str, cfg: dict):
         path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
         inputs = open(path_to_file, 'rb').read().decode(encoding='cp1252')
     else:
-        # dataset = pd.read_excel(path_to_file)
-        # inputs = dataset[dataset['Zwaartepunt'] == 'Programmatuur'][cfg['subject']].values
-        with open('aanleiding.txt', 'r', encoding='utf8', errors='ignore') as f:
-            texts = [f.read()]
-
-    
-    # Tokenize inputs
-    tokenizer = Tokenizer(filters='', 
-                          lower=cfg['word_level'],
-                          char_level=cfg['word_level'])
-    tokenizer.fit_on_texts(texts)
-    sequences = tokenizer.texts_to_sequences(texts)
-
-    # Limit vocab to max_words
-    max_words = cfg['max_words']
-    reverse_word_map = {k: v for (k, v) in tokenizer.word_index.items() if v <= max_words}
-
-    if cfg.get('single_text', False): 
-        tokenizer.word_index[META_TOKEN] = len(tokenizer.word_index) + 1
-
-    # Create vocabulary
-    vocab = tokenizer.word_index
-    vocab_size = len(tokenizer.word_index)
-    indices_char = dict((vocab[c], c) for c in vocab)
-
-    print('Vocabulary size in this corpus: ', vocab_size)
-
-    # if cfg['word_level']:
-    #     # If training word level, must add spaces around each
-    #     # punctuation. https://stackoverflow.com/a/3645946/9314418
-    #     punct = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\\n\\t\'‘’“”’–—…'
-    #     for i in range(len(texts)):
-    #         texts[i] = re.sub('([{}])'.format(punct), r' \1 ', texts[i])
-    #         texts[i] = re.sub(' {2,}', ' ', texts[i])
-    #     texts = [text_to_word_sequence(text, filters='') for text in texts]
-    
-    # # Create training data
-    # X_train, X_val, y_train, y_val = split_data(texts, vocab_size, cfg)
-
-    # Model parameters.
-    if cfg['optimizer'] == 'adam':
-        optimizer = optimizers.Adam(learning_rate=cfg['learning_rate'])
-    elif cfg['optimizer'] == 'sgd':
-        optimizer = optimizers.SGD(learning_rate=cfg['learning_rate'])
-    else:
-        raise TypeError("Specified optimizer type does not exist.")
-
-    if cfg['loss_type'] == 'ce':
-        loss = 'categorical_crossentropy'
-    else:
-        raise TypeError("Unrecognized loss type.")
+        dataset = pd.read_excel(path_to_file)
+        texts = dataset[dataset['Zwaartepunt'] == 'Programmatuur'][cfg['subject']].values
 
     # Set up results folder
     time_now = datetime.now()
@@ -113,57 +62,29 @@ def train_model(results_path: Path, path_to_file: str, cfg: dict):
     results_dir.mkdir(exist_ok=True, parents=True)
     cfg['results_dir'] = str(results_dir)
 
-    # Callback list contains ModelCheckpoint, CSVlogger and Tensorboard
-    callbacks_list = checkpoint_log(results_dir, loss)
+    # Original code from Max Woolf (Github: Minimaxir):
+    # https://github.com/minimaxir/textgenrnn
+    model = textgenrnn(model_folder=str(results_dir))
+    cfg['name'] = model.config['name']
 
-    if cfg['text_genrnn']:
-        # Original code from Max Woolf (Github: Minimaxir):
-        # https://github.com/minimaxir/textgenrnn
+    train_function = model.train_from_file if cfg['line_delimited'] else model.train_from_largetext_file
 
-        model = textgenrnn(model_folder=str(results_dir))
-
-        train_function = model.train_from_file if cfg['line_delimited'] else model.train_from_largetext_file
-
-        train_function(
-            file_path='aanleiding.txt',
-            new_model=True,
-            num_epochs=cfg['epochs'],
-            gen_epochs=cfg['gen_epochs'],
-            batch_size=cfg['batch_size'],
-            train_size=1-cfg['test_size'],
-            dropout=cfg['dropout'],
-            validation=cfg['validation'],
-            is_csv=cfg['is_csv'],
-            rnn_layers=cfg['rnn_layers'],
-            rnn_size=cfg['rnn_size'],
-            rnn_bidirectional=cfg['rnn_bidirectional'],
-            max_length=cfg['sentence_length'],
-            dim_embeddings=100,
-            word_level=cfg['word_level'])           
-
-    else:
-        # Define model
-        model = build_simple_model(
-            rnn_size=cfg['rnn_size'], 
-            rnn_layers=cfg['rnn_layers'], 
-            vocab_size=vocab_size, 
-            train_len=cfg['sentence_length'] - 1, 
-            dropout=cfg['dropout'],
-            activation=cfg['activation'],
-            optimizer=optimizer,
-            loss=loss,
-            metrics=cfg['metrics'])
-
-        # Train model
-        history = model.fit(X_train,
-                            y_train,
-                            epochs=cfg['epochs'],
-                            batch_size=cfg['batch_size'],
-                            callbacks=callbacks_list,
-                            verbose=2,
-                            validation_data=(X_val, y_val))
-
-        cfg = save_config(model, tokenizer, cfg, reverse_word_map, results_directory)           # Wont work yet with textgenrnn
+    train_function(
+        file_path='aanleiding.txt',
+        new_model=True,
+        num_epochs=cfg['epochs'],
+        gen_epochs=cfg['gen_epochs'],
+        batch_size=cfg['batch_size'],
+        train_size=1-cfg['test_size'],
+        dropout=cfg['dropout'],
+        validation=cfg['validation'],
+        is_csv=cfg['is_csv'],
+        rnn_layers=cfg['rnn_layers'],
+        rnn_size=cfg['rnn_size'],
+        rnn_bidirectional=cfg['rnn_bidirectional'],
+        max_length=cfg['sentence_length'],
+        dim_embeddings=100,
+        word_level=cfg['word_level'])           
 
     return model, cfg
 
@@ -174,35 +95,21 @@ if __name__ == "__main__":
     model, config = train_model(results_path=parameters.results_path,
                                 path_to_file=parameters.path_to_file,
                                 cfg=parameters.__dict__)
-    
+
     if parameters.generate:
-        if parameters.text_genrnn:  
-            if config['line_delimited']:
-                n = 1000
-                max_gen_length = 60 if config['word_level'] else 300
-            else:
-                n = 1
-                max_gen_length = 2000 if config['word_level'] else 1000
-                
-            gen_file = '{}/gen_text.txt'.format(config['results_dir'])
-            temperature =[float(t) for t in config['temperature']]
-
-            model.generate_to_file(gen_file,
-                                    temperature=temperature,
-                                    prefix=config['input_string'],
-                                    n=n,
-                                    max_gen_length=max_gen_length)
+        if config['line_delimited']:
+            n = 1000
+            max_gen_length = 60 if config['word_level'] else 300
         else:
-            with open(config['tokenizer_path'], 'rb') as handle:
-                tokenizer = pickle.load(handle)
-
-            gen_text = generate_text(model=model,
-                                    tokenizer=tokenizer,
-                                    inputs=parameters.input_string,
-                                    reverse_word_map=config['reverse_word_map'],
-                                    train_len=config['sentence_length'] - 1,
-                                    max_len=parameters.sequence_length
-                                    )
+            n = 1
+            max_gen_length = 2000 if config['word_level'] else 1000
             
-            with open(config['results_dir'] + '/gen_text.txt', 'w') as f:
-                f.write(gen_text)
+        gen_file = '{}/gen_text.txt'.format(config['results_dir'])
+        temperature =[float(t) for t in config['temperature']]
+
+        model.generate_to_file(gen_file,
+                                temperature=temperature,
+                                prefix=config['input_string'],
+                                n=n,
+                                max_gen_length=max_gen_length)
+        
