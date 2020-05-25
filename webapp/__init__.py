e8615ed0a15ec8984d1586894a35b72b97134cf6
LuuckyG <luuk.geelen@hotmail.com>
2020-05-25 13:14:05 +0200

    Added custom error handlers

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -30,10 +30,12 @@ def create_app(config_class=Config):
     from webapp.main.routes import main
     from webapp.users.routes import users
     from webapp.searches.routes import searches
+    from webapp.errors.handlers import errors
 
     app.register_blueprint(main)
     app.register_blueprint(users)
     app.register_blueprint(searches)
+    app.register_blueprint(errors)
 
     # with app.app_context():
     #     db.drop_all()
