905409ba8f93a8a3567182c0bb6256267fea69d8
LuuckyG <luuk.geelen@hotmail.com>
2020-06-03 16:35:20 +0200

    Made base test class with setup and teardown methods

--- webapp/tests/test_basic.py
+++ webapp/tests/test_basic.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 import unittest
 
 from webapp import create_app, db, mail
@@ -7,35 +6,23 @@ from webapp.config import TestingConfig
 
 
 class BasicTest(unittest.TestCase):
- 
-    ### Setup and teardown ###
- 
-    # Executed prior to each test
+    
     def setUp(self):
-        self.app = create_app(TestingConfig)
-        print(self.app.config['SECRET_KEY'])
+        self.app = create_app(config_class=TestingConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
-        # self.client = self.app.test_client()
-
-        db.create_all()
+         
+        self.db = db
+        self.db.create_all()
  
         # Disable sending emails during unit testing
         mail.init_app(self.app)
         self.assertEqual(self.app.debug, False)
  
-    # Executed after each test
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        self.db.session.remove()
+        self.db.drop_all()
         self.app_context.pop()
 
-    # Tests
-    def test_main_page(self):
-        with self.app.test_client() as client:
-            response = client.get('/', follow_redirects=True)
-            self.assertEqual(response.status_code, 200)
- 
- 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
