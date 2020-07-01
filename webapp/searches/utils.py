3c4e0de0d8faac699583bacd154431ac0706bf0d
LuuckyG <luuk.geelen@hotmail.com>
2020-07-01 12:32:33 +0200

    Fixed function to combine search query input correctly

--- webapp/searches/utils.py
+++ webapp/searches/utils.py
@@ -1,6 +1,26 @@
+FIELDS_OF_INTEREST = ['key_terms', 
+                      'title', 
+                      'aanleiding',
+                      'opl',
+                      't_knel',
+                      'prog',
+                      't_nieuw']
+
+
 def combine_search_form_inputs(inputs):
     search_query = ''
-    for column_input in inputs:
-        if column_input != '_sa_instance_state' and column_input != 'id':
-            search_query += column_input + ' '
+
+    for field in FIELDS_OF_INTEREST:
+        try:
+            value = inputs[field]
+            if not value == '':
+
+                if not isinstance(value, str):
+                    value = str(value)
+
+                search_query += value + '.\n'
+
+        except KeyError:
+            continue
+
     return search_query
